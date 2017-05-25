b905b03ce53a4afd133411bacfb6bd61deac2295
Luca <lucam@gidiautomazione.it>
2017-05-25 15:01:06 +0200

    fine sessione, tutto funzionante

--- DataExtraction/DataExtraction/FormHome.Designer.cs
+++ DataExtraction/DataExtraction/FormHome.Designer.cs
@@ -46,10 +46,22 @@
             this.listBoxTavole = new System.Windows.Forms.ListBox();
             this.buttonFiltraColonna = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
             this.panel4.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRisultatoRicerca)).BeginInit();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel8.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBoxElencoDataBase
@@ -58,9 +70,9 @@
             this.listBoxElencoDataBase.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBoxElencoDataBase.FormattingEnabled = true;
             this.listBoxElencoDataBase.ItemHeight = 17;
-            this.listBoxElencoDataBase.Location = new System.Drawing.Point(12, 68);
+            this.listBoxElencoDataBase.Location = new System.Drawing.Point(12, 119);
             this.listBoxElencoDataBase.Name = "listBoxElencoDataBase";
-            this.listBoxElencoDataBase.Size = new System.Drawing.Size(260, 259);
+            this.listBoxElencoDataBase.Size = new System.Drawing.Size(260, 208);
             this.listBoxElencoDataBase.TabIndex = 0;
             this.listBoxElencoDataBase.SelectedIndexChanged += new System.EventHandler(this.listBoxElencoDataBase_SelectedIndexChanged);
             // 
@@ -70,9 +82,9 @@
             this.listBoxColonne.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBoxColonne.FormattingEnabled = true;
             this.listBoxColonne.ItemHeight = 17;
-            this.listBoxColonne.Location = new System.Drawing.Point(295, 68);
+            this.listBoxColonne.Location = new System.Drawing.Point(295, 119);
             this.listBoxColonne.Name = "listBoxColonne";
-            this.listBoxColonne.Size = new System.Drawing.Size(260, 412);
+            this.listBoxColonne.Size = new System.Drawing.Size(260, 361);
             this.listBoxColonne.TabIndex = 0;
             this.listBoxColonne.SelectedIndexChanged += new System.EventHandler(this.listBoxColonne_SelectedIndexChanged);
             // 
@@ -169,7 +181,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(785, 12);
+            this.label1.Location = new System.Drawing.Point(785, 13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(164, 26);
             this.label1.TabIndex = 2;
@@ -187,7 +199,8 @@
             // richTextBoxCommentoColonna
             // 
             this.richTextBoxCommentoColonna.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBoxCommentoColonna.Location = new System.Drawing.Point(565, 70);
+            this.richTextBoxCommentoColonna.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxCommentoColonna.Location = new System.Drawing.Point(565, 118);
             this.richTextBoxCommentoColonna.Name = "richTextBoxCommentoColonna";
             this.richTextBoxCommentoColonna.Size = new System.Drawing.Size(1152, 41);
             this.richTextBoxCommentoColonna.TabIndex = 9;
@@ -198,9 +211,9 @@
             this.dataGridViewRisultatoRicerca.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridViewRisultatoRicerca.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Blue;
             dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
@@ -208,9 +221,9 @@
             this.dataGridViewRisultatoRicerca.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewRisultatoRicerca.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
             this.dataGridViewRisultatoRicerca.EnableHeadersVisualStyles = false;
-            this.dataGridViewRisultatoRicerca.Location = new System.Drawing.Point(569, 117);
+            this.dataGridViewRisultatoRicerca.Location = new System.Drawing.Point(565, 165);
             this.dataGridViewRisultatoRicerca.Name = "dataGridViewRisultatoRicerca";
-            this.dataGridViewRisultatoRicerca.Size = new System.Drawing.Size(1147, 623);
+            this.dataGridViewRisultatoRicerca.Size = new System.Drawing.Size(1147, 577);
             this.dataGridViewRisultatoRicerca.TabIndex = 10;
             // 
             // listBoxTavole
@@ -219,39 +232,141 @@
             this.listBoxTavole.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBoxTavole.FormattingEnabled = true;
             this.listBoxTavole.ItemHeight = 17;
-            this.listBoxTavole.Location = new System.Drawing.Point(12, 341);
+            this.listBoxTavole.Location = new System.Drawing.Point(12, 375);
             this.listBoxTavole.Name = "listBoxTavole";
-            this.listBoxTavole.Size = new System.Drawing.Size(260, 259);
+            this.listBoxTavole.Size = new System.Drawing.Size(260, 225);
             this.listBoxTavole.TabIndex = 11;
             this.listBoxTavole.SelectedIndexChanged += new System.EventHandler(this.listBoxTavole_SelectedIndexChanged);
             // 
             // buttonFiltraColonna
             // 
             this.buttonFiltraColonna.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonFiltraColonna.Location = new System.Drawing.Point(295, 495);
+            this.buttonFiltraColonna.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.buttonFiltraColonna.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonFiltraColonna.ForeColor = System.Drawing.Color.Blue;
+            this.buttonFiltraColonna.Location = new System.Drawing.Point(295, 483);
             this.buttonFiltraColonna.Name = "buttonFiltraColonna";
-            this.buttonFiltraColonna.Size = new System.Drawing.Size(259, 52);
+            this.buttonFiltraColonna.Size = new System.Drawing.Size(260, 59);
             this.buttonFiltraColonna.TabIndex = 12;
             this.buttonFiltraColonna.Text = "Filtra Singola Colonna";
-            this.buttonFiltraColonna.UseVisualStyleBackColor = true;
+            this.buttonFiltraColonna.UseVisualStyleBackColor = false;
             this.buttonFiltraColonna.Click += new System.EventHandler(this.buttonFiltraColonna_Click);
             // 
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(295, 553);
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Blue;
+            this.button2.Location = new System.Drawing.Point(295, 545);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(259, 52);
+            this.button2.Size = new System.Drawing.Size(260, 59);
             this.button2.TabIndex = 13;
             this.button2.Text = "Elenco Tavola Completa";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Controls.Add(this.label5);
+            this.panel5.Location = new System.Drawing.Point(12, 87);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(260, 32);
+            this.panel5.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(81, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 24);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Database";
+            // 
+            // panel6
+            // 
+            this.panel6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel6.Controls.Add(this.label6);
+            this.panel6.Location = new System.Drawing.Point(295, 87);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(260, 32);
+            this.panel6.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(86, 3);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 24);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Colonne";
+            // 
+            // panel7
+            // 
+            this.panel7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel7.Controls.Add(this.label7);
+            this.panel7.Location = new System.Drawing.Point(12, 344);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(260, 32);
+            this.panel7.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(94, 3);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 24);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Tavole";
+            // 
+            // panel8
+            // 
+            this.panel8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel8.Controls.Add(this.label8);
+            this.panel8.Location = new System.Drawing.Point(565, 87);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(1152, 32);
+            this.panel8.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(394, 3);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(363, 24);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Informazioni sulla colonna selezionata";
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1729, 778);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.buttonFiltraColonna);
             this.Controls.Add(this.listBoxTavole);
@@ -275,6 +390,14 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRisultatoRicerca)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -298,6 +421,14 @@
         private System.Windows.Forms.ListBox listBoxTavole;
         private System.Windows.Forms.Button buttonFiltraColonna;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Label label8;
     }
 }
 
